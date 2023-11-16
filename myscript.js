89241ee05bd13bd21188a7d1556a080de3b29d23
Pasqualibruno <pasqualinomolosso@gmail.com>
2023-11-16 16:38:14 +0100

    end

---
+++ myscript.js
@@ -0,0 +1,27 @@
+/*
+-variabile
+-pront
+-console.loge
+-messageElement
+-getElementById
+*/ 
+//chiedere il nome
+const your_name = prompt("Qual'è il tuo nome?");
+console.log(your_name);
+//stampo in pagina del nome
+/*const messageElement = document.getElementById('name')
+messageElement.innerHTML = `Il tuo nome é ${your_name}`*/
+
+//chiedere il cognome
+const your_last_name = prompt("Qual'è il tuo cognome?");
+console.log(your_last_name);
+//stampo in pagina del nome
+/*const messageElement = document.getElementById('last-name')
+messageElement.innerHTML = `Il tuo cognome é ${your_last_name}`*/
+
+//chiedere il colore preferito
+const your_favorite_color = prompt("Qual'è il tuo colore preferito?");
+console.log(your_favorite_color);
+//generazione della password
+const messageElement = document.getElementById('genered-password')
+messageElement.innerHTML = `La tua passwoed è ${your_name}${your_last_name}${your_favorite_color}23`
